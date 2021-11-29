de6b882ee13ba3497eb17298b6f0ab6252f146ac
Marc Asendorf <marc2@uni-bremen.de>
2021-11-29 17:26:05 +0100

    add a little more complex test

--- tests/test_move.nim
+++ tests/test_move.nim
@@ -2,7 +2,7 @@ import unittest
 import ../src/model/board
 import ../src/model/pieces
 
-test "test pawn single step":
+test "pawn single step":
   var
     b = populateBoard()
 
@@ -11,5 +11,24 @@ test "test pawn single step":
     input = @["a7", "a6"]
 
   check(p.yPos == 1)
-  check(move(input, b, Black))
+  check(move(input, b, Black)) # == true
   check(p.yPos == 2)
+
+test "bishop NW single step":
+  var
+    b = populateBoard()
+
+  let
+    p = b.board[6][4] # pawn
+    bishop = b.board[7][5]
+    input = @["e2", "e3", "f1", "e2"]
+
+  check(p.yPos == 6)
+  check(bishop.yPos == 7)
+  check(bishop.xPos == 5)
+  check(move(@[input[0], input[1]], b, White))
+  check(p.yPos == 5)
+  check(move(@[input[2], input[3]], b, White))
+  check(bishop.yPos == 6)
+  check(bishop.xPos == 4)
+
