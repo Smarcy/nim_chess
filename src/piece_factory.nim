c08b4f008dac2aca3389229ae890cc94cb68d8af
Marc Asendorf <marc2@uni-bremen.de>
2021-11-29 20:09:37 +0100

    more tests but wtf how to cleanup after every test?

--- src/piece_factory.nim
+++ src/piece_factory.nim
@@ -2,62 +2,62 @@ import model/pieces
 
 proc createPawns(): seq[Pawn] =
 
-  let wPawn1 = newPawn(White, 0, 6)
-  let wPawn2 = newPawn(White, 1, 6)
-  let wPawn3 = newPawn(White, 2, 6)
-  let wPawn4 = newPawn(White, 3, 6)
-  let wPawn5 = newPawn(White, 4, 6)
-  let wPawn6 = newPawn(White, 5, 6)
-  let wPawn7 = newPawn(White, 6, 6)
-  let wPawn8 = newPawn(White, 7, 6)
+  var wPawn1 = newPawn(White, 0, 6)
+  var wPawn2 = newPawn(White, 1, 6)
+  var wPawn3 = newPawn(White, 2, 6)
+  var wPawn4 = newPawn(White, 3, 6)
+  var wPawn5 = newPawn(White, 4, 6)
+  var wPawn6 = newPawn(White, 5, 6)
+  var wPawn7 = newPawn(White, 6, 6)
+  var wPawn8 = newPawn(White, 7, 6)
 
-  let bPawn1 = newPawn(Black, 0, 1)
-  let bPawn2 = newPawn(Black, 1, 1)
-  let bPawn3 = newPawn(Black, 2, 1)
-  let bPawn4 = newPawn(Black, 3, 1)
-  let bPawn5 = newPawn(Black, 4, 1)
-  let bPawn6 = newPawn(Black, 5, 1)
-  let bPawn7 = newPawn(Black, 6, 1)
-  let bPawn8 = newPawn(Black, 7, 1)
+  var bPawn1 = newPawn(Black, 0, 1)
+  var bPawn2 = newPawn(Black, 1, 1)
+  var bPawn3 = newPawn(Black, 2, 1)
+  var bPawn4 = newPawn(Black, 3, 1)
+  var bPawn5 = newPawn(Black, 4, 1)
+  var bPawn6 = newPawn(Black, 5, 1)
+  var bPawn7 = newPawn(Black, 6, 1)
+  var bPawn8 = newPawn(Black, 7, 1)
 
   result = @[wPawn1, wPawn2, wPawn3, wPawn4, wPawn5, wPawn6, wPawn7, wPawn8,
       bPawn1, bPawn2, bPawn3, bPawn4, bPawn5, bPawn6, bPawn7, bPawn8]
 
 proc createBishops(): seq[Bishop] =
-  let wBishop1 = newBishop(White, 2, 7)
-  let wBishop2 = newBishop(White, 5, 7)
+  var wBishop1 = newBishop(White, 2, 7)
+  var wBishop2 = newBishop(White, 5, 7)
 
-  let bBishop1 = newBishop(Black, 2, 0)
-  let bBishop2 = newBishop(Black, 5, 0)
+  var bBishop1 = newBishop(Black, 2, 0)
+  var bBishop2 = newBishop(Black, 5, 0)
 
   result = @[wBishop1, wBishop2, bBishop1, bBishop2]
 
 proc createKnights(): seq[Knight] =
-  let wKnight1 = newKnight(White, 1, 7)
-  let wKnight2 = newKnight(White, 6, 7)
+  var wKnight1 = newKnight(White, 1, 7)
+  var wKnight2 = newKnight(White, 6, 7)
 
-  let bKnight1 = newKnight(Black, 1, 0)
-  let bKnight2 = newKnight(Black, 6, 0)
+  var bKnight1 = newKnight(Black, 1, 0)
+  var bKnight2 = newKnight(Black, 6, 0)
 
   result = @[wKnight1, wKnight2, bKnight1, bKnight2]
 
 proc createRooks(): seq[Rook] =
-  let wRook1 = newRook(White, 0, 7)
-  let wRook2 = newRook(White, 7, 7)
+  var wRook1 = newRook(White, 0, 7)
+  var wRook2 = newRook(White, 7, 7)
 
-  let bRook1 = newRook(Black, 0, 0)
-  let bRook2 = newRook(Black, 7, 0)
+  var bRook1 = newRook(Black, 0, 0)
+  var bRook2 = newRook(Black, 7, 0)
 
   result = @[wRook1, wRook2, bRook1, bRook2]
 
 proc createQueens(): seq[Queen] =
-  let wQueen = newQueen(White, 3, 7)
-  let bQueen = newQueen(Black, 3, 0)
+  var wQueen = newQueen(White, 3, 7)
+  var bQueen = newQueen(Black, 3, 0)
   result = @[wQueen, bQueen]
 
 proc createKings(): seq[King] =
-  let wKing = newKing(White, 4, 7)
-  let bKing = newKing(Black, 4, 0)
+  var wKing = newKing(White, 4, 7)
+  var bKing = newKing(Black, 4, 0)
   result = @[wKing, bKing]
 
 proc createFreeTiles(): seq[FreeTile] =
--- tests/test_move.nim
+++ tests/test_move.nim
@@ -14,6 +14,10 @@ test "pawn single step":
   check(move(input, b, Black)) # == true
   check(p.yPos == 2)
 
+  #cleanup..
+  b.board[1][0] = newPawn(Black, 0, 1)
+  p.yPos = 1
+
 test "bishop NW single step":
   var
     b = populateBoard()
@@ -23,6 +27,7 @@ test "bishop NW single step":
     bishop = b.board[7][5]
     input = @["e2", "e3", "f1", "e2"]
 
+  b.draw()
   check(p.yPos == 6)
   check(bishop.yPos == 7)
   check(bishop.xPos == 5)
@@ -31,4 +36,32 @@ test "bishop NW single step":
   check(move(@[input[2], input[3]], b, White))
   check(bishop.yPos == 6)
   check(bishop.xPos == 4)
+  check(b.board[6][4] == bishop)
+
+  #cleanup .. wtf there has to be a better way?!
+  bishop.yPos = 7
+  bishop.xPos = 5
+  p.yPos = 6
+  b.board[6][4] = newPawn(White, 4, 6)
+
+
+test "bishop NE single step":
+  var
+    b = populateBoard()
+
+  let
+    p = b.board[6][6] # pawn
+    bishop = b.board[7][5]
+    input = @["g2", "g3", "f1", "g2"]
+
+  b.draw()
+  check(p.yPos == 6)
+  check(bishop.yPos == 7)
+  check(bishop.xPos == 5)
+  check(move(@[input[0], input[1]], b, White))
+  check(p.yPos == 5)
+  check(move(@[input[2], input[3]], b, White))
+  check(bishop.yPos == 6)
+  check(bishop.xPos == 6)
+  check(b.board[6][6] == bishop)
 
